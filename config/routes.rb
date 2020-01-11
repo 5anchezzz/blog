c670aba2d5063d0a1f01640dcbc9ef8be4ad040f
5anchezzz <atomicheart@mail.ru>
2020-01-11 23:48:00 +0300

    working blog

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'posts#index'
+  resources :posts, only: [:show, :index]
+  resources :tags, only: [:show]
+  resources :categories, only: [:show]
+
+  namespace :admin do
+    resources :categories, except: [:show]
+    resources :posts, except: [:show, :index]
+  end
+
+end
